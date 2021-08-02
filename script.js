76e6e2d46db3c6eccc467deb14ab69c405efb05f
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-01 19:10:21 -0600

    Created diiferent function structure and began working on method of actually generating the password

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
 
 // Write password to the #password input
 function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
   
-  var characterValue = prompt("Please put in a numeric value between 8 and 128 for your desired lenght password!");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
+
+function lenght() {
+    var characterValue = prompt("Please put in a numeric value between 8 and 128 for your desired lenght password!");
   var characterValuedenied = ("abcdefghijklmnopqrstuvwxyz!@#$%^&*()<>?/;':[]{}\| ").split(" ");
   console.log(characterValue);
   if (characterValuedenied.includes(characterValue)) {
@@ -19,18 +26,26 @@ function writePassword() {
     }
  else if(characterValue < 8) {
       alert("Please choose a value greater then or equal to 8 but less than or equal 128");
-      return writePassword();
+      return Password();
   } 
 
+    generatePassword(); 
+} 
 
+    function generatePassword() {
+    
   var lowerConfirm = confirm("Would you like to include lower case letters in your password?");
   var UpperConfirm = confirm("Would you like to include upper case letters in your password?");
   var numericConfirm = confirm("Would you like to include numeric characters in your password?");
-  var specialCOnfirm = confirm("Would you like to include special characters in your password?");
+  var specialConfirm = confirm("Would you like to include special characters in your password?");
+    
+  if(!lowerConfirm && !UpperConfirm && !numericConfirm && !specialConfirm) {
+      alert("Please choose at least one character type!");
+      return generatePassword();
+  }
 
-//   passwordText.value = password;
+    }
 
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", length);
