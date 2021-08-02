b6776892e9fe005e543b5b03259fd045dac208ff
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-01 21:17:02 -0600

    Created functional 'random' generator

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lower = "abcdefghijklmnopqrstuvwxyz"
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numeric = "1234567890"
-var special = "!@#$%^&*()_-[]{}\|;:',./?+="
+var lower = "abcdefghijklmnopqrstuvwxyz".split();
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split();
+var numeric = "1234567890".split();
+var special = "!@#$%^&*()_-[]{}\|;:',./?+=".split();
 var userSelection = "";
 
 // Write password to the #password input
 function writePassword() { 
-    var password = generatePassword();
+    var password = generatePassword;
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
 }
 
-function passwordLength() {
+function generatePassword() {
+    var result = " ";
     var characterValue = prompt("Please put in a numeric value between 8 and 128 for your desired lenght password!");
   console.log(characterValue);
   if (isNaN(characterValue)) {
       alert("Please input a numeric value between 8 and 128!") 
-      return passwordLength();   
+      return generatePassword();   
   } else if (characterValue > 128 || characterValue < 8) {
       alert("Please input a number greater than or equal to 8 and less than or equal to 128!");
-      return passwordLength();
-  }
-  generatePassword();
-}
-    function generatePassword() {
-    
+      return generatePassword();
+  }   
   var lowerConfirm = confirm("Would you like to include lower case letters in your password?");
-  var UpperConfirm = confirm("Would you like to include upper case letters in your password?");
+  var upperConfirm = confirm("Would you like to include upper case letters in your password?");
   var numericConfirm = confirm("Would you like to include numeric characters in your password?");
   var specialConfirm = confirm("Would you like to include special characters in your password?");
     
-  if(!lowerConfirm && !UpperConfirm && !numericConfirm && !specialConfirm) {
+  if(!lowerConfirm && !upperConfirm && !numericConfirm && !specialConfirm) {
       alert("Please choose at least one character type!");
       return generatePassword();
   }
@@ -50,12 +47,16 @@ function passwordLength() {
   if(specialConfirm) {
       userSelection += special
   }
+  for (var i = 0; i < characterValue; i++) {
+    result += userSelection.charAt(Math.floor(Math.random() * userSelection.length)); 
+  }
+  console.log(result);
+     return result;   
 
-  
   }
 
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", passwordLength)
+generateBtn.addEventListener("click", generatePassword)
